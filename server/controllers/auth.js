dc162a3f498299dee837457c533483e8e0069807
DangAnhVu <danganhvupro3z@gmail.com>
2025-10-24 17:04:00 +0700

    feat: postRoutes-setup

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,7 +1,6 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
-import { use } from "react";
 
 // Đăng ký người dùng
 export const register = async (req, res) => {
