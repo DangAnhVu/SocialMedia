0925f65e6529b0e0ea96cb0dd2d00460cf9086f7
DangAnhVu <danganhvupro3z@gmail.com>
2025-10-28 15:52:43 +0700

    setup-theme-darkmode-navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,36 @@
+/* eslint-disable no-unused-vars */
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "@/screens/homepage";
 import LoginPage from "@/screens/loginPage";
 import Navbar from "@/screens/navbar";
 import ProfilePage from "@/screens/profilePage";
 
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+
 function App() {
+    const mode = useSelector((state) => state.mode); // lấy giá trị mode từ Redux store
+    const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]); // tạo theme dựa trên mode
+
     return (
         <div className="app">
-            <p>Hello World</p>
             <BrowserRouter>
-                <Navbar />
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/login" element={<LoginPage />} />
-                    <Route path="/profile/:userId" element={<ProfilePage />} />
-                    <Route path="*" element={<Navigate to="/" />} />
-                </Routes>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <Navbar />
+                    <Routes>
+                        <Route path="/" element={<HomePage />} />
+                        <Route path="/login" element={<LoginPage />} />
+                        <Route
+                            path="/profile/:userId"
+                            element={<ProfilePage />}
+                        />
+                        <Route path="*" element={<Navigate to="/" />} />
+                    </Routes>
+                </ThemeProvider>
             </BrowserRouter>
         </div>
     );
--- client/src/screens/navbar/index.jsx
+++ client/src/screens/navbar/index.jsx
@@ -1,8 +1,205 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+import {
+    Box,
+    IconButton,
+    InputBase,
+    Typography,
+    Select,
+    MenuItem,
+    FormControl,
+    useTheme,
+    useMediaQuery,
+} from "@mui/material";
+import {
+    Search,
+    Message,
+    DarkMode,
+    LightMode,
+    Notifications,
+    Help,
+    Menu,
+    Close,
+} from "@mui/icons-material";
+import { useDispatch, useSelector } from "react-redux";
+import { setMode, setLogout } from "@/state/index";
+import { useNavigate } from "react-router-dom";
+import FlexBetween from "@/components/FlexBetween";
+
 const Navbar = () => {
+    const dispatch = useDispatch(); // Khởi tạo dispatch để gửi action đến Redux store
+    const navigate = useNavigate(); // Khởi tạo navigate để điều hướng giữa các trang
+    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)"); // Kiểm tra kích thước màn hình để hiển thị navbar phù hợp
+    const user = useSelector((state) => state.user); // Lấy thông tin user từ Redux store
+    const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false); // Trạng thái hiển thị menu trên thiết bị di động
+    const theme = useTheme(); // Lấy theme từ MUI
+    const neutralLight = theme.palette.neutral.light; // Màu sắc từ theme
+    const dark = theme.palette.neutral.dark;
+    const background = theme.palette.background.default;
+    const primaryLight = theme.palette.primary.light;
+    const alt = theme.palette.background.alt;
+
+    // const fullName = `${user.firstName} ${user.lastName}`; // Tên đầy đủ của user
+    const fullName = user ? `${user.firstName} ${user.lastName}` : "Guest"; // Tên đầy đủ của user hoặc "Guest" nếu user không tồn tại
+
     return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
+        <FlexBetween padding="1rem 6%" backgroundColor={alt}>
+            <FlexBetween gap="1.75rem">
+                <Typography
+                    fontWeight="bold"
+                    fontSize="clamp(1rem, 2rem, 2.25rem)" //
+                    color="primary"
+                    onClick={() => navigate("/")}
+                    sx={{
+                        "&:hover": { color: primaryLight, cursor: "pointer" }, // Hiệu ứng hover cho Typography
+                    }}
+                >
+                    SocialMedia
+                </Typography>
+                {isNonMobileScreens && (
+                    <FlexBetween
+                        backgroundColor={neutralLight}
+                        borderRadius="9px"
+                        gap="3rem"
+                        padding="0.1rem 1.5rem"
+                    >
+                        <InputBase placeholder="Search..." />
+                        <IconButton>
+                            <Search />
+                        </IconButton>
+                    </FlexBetween>
+                )}
+            </FlexBetween>
+            {/* DESKTOP NAV */}
+            {isNonMobileScreens ? (
+                <FlexBetween gap="2rem">
+                    <IconButton onClick={() => dispatch(setMode())}>
+                        {theme.palette.mode === "dark" ? (
+                            <DarkMode sx={{ fontSize: "25px" }} />
+                        ) : (
+                            <LightMode sx={{ fontSize: "25px" }} />
+                        )}
+                    </IconButton>
+                    <Message sx={{ fontSize: "25px" }} />
+                    <Notifications sx={{ fontSize: "25px" }} />
+                    <Help sx={{ fontSize: "25px" }} />
+                    <FormControl variant="standard" value={fullName}>
+                        <Select
+                            value={fullName}
+                            sx={{
+                                backgroundColor: neutralLight,
+                                width: "150px",
+                                borderRadius: "0.25rem",
+                                p: "0.25rem 1rem",
+                                "& .MuiSvgIcon-root": {
+                                    pr: "0.25rem",
+                                    width: "3rem",
+                                },
+                                "& .MuiSelect-select:focus": {
+                                    backgroundColor: neutralLight,
+                                },
+                            }}
+                            input={<InputBase />}
+                        >
+                            <MenuItem value={fullName}>
+                                <Typography>{fullName}</Typography>
+                            </MenuItem>
+                            <MenuItem value="profile" onClick={() => {}}>
+                                My Profile
+                            </MenuItem>
+                            <MenuItem
+                                onClick={() => dispatch(setLogout())}
+                                value="logout"
+                            >
+                                Log Out
+                            </MenuItem>
+                        </Select>
+                    </FormControl>
+                </FlexBetween>
+            ) : (
+                <IconButton
+                    onClick={() => setIsMobileMenuToggled(!isMobileMenuToggled)}
+                >
+                    <Menu />
+                </IconButton>
+            )}
+            {/* MOBILE NAV */}
+            {!isNonMobileScreens && isMobileMenuToggled && (
+                <Box
+                    position="fixed"
+                    right="0"
+                    bottom="0"
+                    height="100%"
+                    zIndex="10"
+                    maxWidth="500px"
+                    minWidth="300px"
+                    backgroundColor={background}
+                >
+                    {/* CLOSE ICON */}
+                    <Box display="flex" justifyContent="flex-end" p="1rem">
+                        <IconButton
+                            onClick={() =>
+                                setIsMobileMenuToggled(!isMobileMenuToggled)
+                            }
+                        >
+                            <Close />
+                        </IconButton>
+                    </Box>
+
+                    {/* MENU ITEMS */}
+                    <FlexBetween
+                        display="flex"
+                        flexDirection="column"
+                        justifyContent="center"
+                        alignItems="center"
+                        gap="3rem"
+                    >
+                        <IconButton onClick={() => dispatch(setMode())}>
+                            {theme.palette.mode === "dark" ? (
+                                <DarkMode sx={{ fontSize: "25px" }} />
+                            ) : (
+                                <LightMode sx={{ fontSize: "25px" }} />
+                            )}
+                        </IconButton>
+                        <Message sx={{ fontSize: "25px" }} />
+                        <Notifications sx={{ fontSize: "25px" }} />
+                        <Help sx={{ fontSize: "25px" }} />
+                        <FormControl variant="standard" value={fullName}>
+                            <Select
+                                value={fullName}
+                                sx={{
+                                    backgroundColor: neutralLight,
+                                    width: "150px",
+                                    borderRadius: "0.25rem",
+                                    p: "0.25rem 1rem",
+                                    "& .MuiSvgIcon-root": {
+                                        pr: "0.25rem",
+                                        width: "3rem",
+                                    },
+                                    "& .MuiSelect-select:focus": {
+                                        backgroundColor: neutralLight,
+                                    },
+                                }}
+                                input={<InputBase />}
+                            >
+                                <MenuItem value={fullName}>
+                                    <Typography>{fullName}</Typography>
+                                </MenuItem>
+                                <MenuItem value="profile" onClick={() => {}}>
+                                    My Profile
+                                </MenuItem>
+                                <MenuItem
+                                    onClick={() => dispatch(setLogout())}
+                                    value="logout"
+                                >
+                                    Log Out
+                                </MenuItem>
+                            </Select>
+                        </FormControl>
+                    </FlexBetween>
+                </Box>
+            )}
+        </FlexBetween>
     );
 };
 
